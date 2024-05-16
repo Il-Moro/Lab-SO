506644492623c59574bdbe3a7bba9f9cdea27a87
Il-Moro <filmor04@gmail.com>
2024-05-16 13:27:07 +0200

    More files for new lab 3

---
+++ parallel.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <sys/types.h>
+
+/*
+Funzione di sostitusione della stringa:
+
+
+
+
+*/
+
+
+int main(int argc, char * argv){
+    
+    if(argc != 4){
+        perror("Error");
+        eixit(1);
+    }
+
+}
